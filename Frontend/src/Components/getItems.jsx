2ed0e594a3884697a8e77c4c1ab6dbc2fedbdc90
TinkeshDeshmukh <149570723+TinkeshDeshmukh@users.noreply.github.com>
2025-06-22 10:34:27 +0530

    Update getItems.jsx

--- Frontend/src/Components/getItems.jsx
+++ Frontend/src/Components/getItems.jsx
@@ -9,7 +9,7 @@ const GetItems = () => {
     useEffect(() => {
         async function fetchItems() {
             try {
-                const response = await axios.get("http://localhost:3000/api/data/fetch", {
+                const response = await axios.get("https://assesment-ldj4.onrender.com/api/data/fetch", {
                     headers: {
                         Authorization: `${localStorage.getItem("token")}`,
                     },
