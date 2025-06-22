f9c7a79490a15abf3b42e4b22a486c508d22ae9b
TinkeshDeshmukh <149570723+TinkeshDeshmukh@users.noreply.github.com>
2025-06-22 11:26:02 +0530

    Update Server.js

--- Backend/Server.js
+++ Backend/Server.js
@@ -9,17 +9,7 @@ env.config()
 
 app.use(express.json());
 // Handling cross origin error 
-const corsOptions = {
-  origin: ['https://assesment-1-87vk.onrender.com'],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-// ✅ CORS MIDDLEWARE
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // preflight handler
-
+app.use(cors())
 mongoose.connect(process.env.MongoURL )
     .then(() => {
         console.log(`Database connected`);
